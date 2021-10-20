dc704c9a7d2413ad63e4a724c8f66ed68e983b31
Austin Parker <austincparker@gmail.com>
2021-10-20 17:39:43 -0500

    form comp

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -1,50 +1,62 @@
-// import React from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { createPlayer } from '../api/data/teamData';
 
-// export default function PlayerForm() {
-//   const [formInput, setFormInput] = useState(initialState);
+const initialState = {
+  name: '',
+  imageUrl: '',
+  position: '',
+};
 
-//   const resetForm = () => {
-//     setFormInput({ ...initialState });
-//   };
+export default function PlayerForm({ setPlayers }) {
+  const [formInput, setFormInput] = useState(initialState);
 
-//   const handleChange = (e) => {
-//     setFormInput((prevState) => ({
-//       ...prevState,
-//       [e.target.name]: e.target.value,
-//     }));
-//   };
+  const resetForm = () => {
+    setFormInput({ ...initialState });
+  };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.warn('add a player');
-//     createPlayer({ ...formInput }).then((players) => {
-//       setPlayers(players);
-//     });
-//     resetForm();
-//   };
+  const handleChange = (e) => {
+    setFormInput((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-//   return (
-//     <div className="text-center">
-//       <form id="playerForm" className="mb-3 d-flex align-items-center">
-//         <label htmlFor="name" className="me-1">
-//           <input
-//             name="name"
-//             id="name"
-//             value={formInput.name}
-//             onChange={handleChange}
-//             required
-//             className="form-control"
-//             placeholder="ADD A PLAYER"
-//           />
-//         </label>
-//         <button
-//           type="submit"
-//           className="btn btn-success"
-//           onClick={handleSubmit}
-//         >
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.warn('add a player');
+    createPlayer({ ...formInput }).then((players) => {
+      setPlayers(players);
+    });
+    resetForm();
+  };
+
+  return (
+    <div className="text-center">
+      <form id="playerForm" className="mb-3 d-flex align-items-center">
+        <label htmlFor="name" className="me-1">
+          <input
+            name="name"
+            id="name"
+            value={formInput.name}
+            onChange={handleChange}
+            required
+            className="form-control"
+            placeholder="ADD A PLAYER"
+          />
+        </label>
+        <button
+          type="submit"
+          className="btn btn-success"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+PlayerForm.propTypes = {
+  setPlayers: PropTypes.func.isRequired,
+};
--- src/views/New.js
+++ src/views/New.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { createPlayer } from '../api/data/teamData';
+import PlayerForm from '../components/PlayerForm';
 
 const initialState = {
   name: '',
@@ -8,56 +8,22 @@ const initialState = {
   position: '',
 };
 
-export default function PlayerForm({ setPlayers }) {
+// eslint-disable-next-line react/prop-types
+export default function New({ setPlayers }) {
   const [formInput, setFormInput] = useState(initialState);
-
-  const resetForm = () => {
-    setFormInput({ ...initialState });
-  };
-
-  const handleChange = (e) => {
-    setFormInput((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.warn('add a player');
-    createPlayer({ ...formInput }).then((players) => {
-      setPlayers(players);
-    });
-    resetForm();
-  };
 
   return (
     <div className="text-center">
       <h1>Add a Player</h1>
-      <form id="playerForm" className="mb-3 d-flex align-items-center">
-        <label htmlFor="name" className="me-1">
-          <input
-            name="name"
-            id="name"
-            value={formInput.name}
-            onChange={handleChange}
-            required
-            className="form-control"
-            placeholder="ADD A PLAYER"
-          />
-        </label>
-        <button
-          type="submit"
-          className="btn btn-success"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-      </form>
+      <PlayerForm
+        setFormInput={setFormInput}
+        setPlayers={setPlayers}
+        formInput={formInput}
+      />
     </div>
   );
 }
 
-PlayerForm.propTypes = {
+New.propTypes = {
   setPlayers: PropTypes.func.isRequired,
 };
