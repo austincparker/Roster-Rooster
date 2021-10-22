37d3cbb84575580cc10d7e428269435159a95ed8
Austin Parker <austincparker@gmail.com>
2021-10-22 13:40:19 -0500

    login logout

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { getTeam } from '../api/data/teamData';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 function Initialize() {
   const [players, setPlayers] = useState([]);
   const [editItem, setEditItem] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getTeam().then(setPlayers);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0],
+        };
+        setUser(userInfoObj);
+        getTeam().then(setPlayers);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
   }, []);
+
   return (
     <div className="App">
-      <Navigation />
-      <Routes
-        players={players}
-        setPlayers={setPlayers}
-        setEditItem={setEditItem}
-        obj={editItem}
-      />
+      {user ? (
+        <>
+          <Navigation />
+          <Routes
+            players={players}
+            setPlayers={setPlayers}
+            setEditItem={setEditItem}
+            obj={editItem}
+          />
+        </>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 }
